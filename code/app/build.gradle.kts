d789834ec11a2b5e565d43284e0236c55c76d6f7
Jagjotbrar13 <jagjotbrar.21@gmail.com>
2025-11-05 18:56:58 -0700

    completed all Organizer xml files.

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -33,10 +33,15 @@ android {
 
 dependencies {
 
+
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
