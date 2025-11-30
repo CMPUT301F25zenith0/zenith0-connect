22346558e16ab6e5ccae73d29df3c801f9c7e737
vjattu <jattu@ualberta.ca>
2025-11-30 04:53:21 -0700

    added lottery criteria button

--- code/app/src/main/java/com/example/connect/activities/EventDetails.java
+++ code/app/src/main/java/com/example/connect/activities/EventDetails.java
@@ -886,7 +886,7 @@ public class EventDetails extends AppCompatActivity {
     private void showLotteryCriteriaInfo() {
         new android.app.AlertDialog.Builder(this)
                 .setTitle("Lottery Criteria")
-                .setMessage("The draw happens randomly on the users on the waiting list.")
+                .setMessage("TThe draw is conducted by randomly selecting a fixed number of entrants from the waiting list, and every entrant has an equal and fair chance of being selected.")
                 .setPositiveButton("Got it", (dialog, which) -> dialog.dismiss())
                 .show();
     }
